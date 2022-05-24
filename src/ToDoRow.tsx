05bfa2e5830dd6d1ee16592410e41cdf5af69e22
lokesh kaloni <lokeshkaloni@gmail.com>
2022-05-24 12:38:37 +0530

    some changes

--- src/ToDoRow.tsx
+++ src/ToDoRow.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from "react";
 import { todoRowProps } from "./TodoType";
+import { v4 as uuidv4 } from "uuid";
 
 const ToDoRow: FC<todoRowProps> = (props) => {
   const onCheckBoxChange = () => {
@@ -8,7 +9,12 @@ const ToDoRow: FC<todoRowProps> = (props) => {
 
   return (
     <div className="ml-4 flex items-center space-x-2">
-      <input checked={props.done} onChange={onCheckBoxChange} type="checkbox" />
+      <input
+        checked={props.done}
+        onChange={onCheckBoxChange}
+        type="checkbox"
+        key={props.key}
+      />
       <h3 className={"text-gray-600 " + (props.done && "line-through")}>
         {props.children}
       </h3>
--- src/TodoType.tsx
+++ src/TodoType.tsx
@@ -3,4 +3,5 @@ import { HtmlHTMLAttributes } from "react";
 export type todoRowProps = {
   done: boolean;
   onStatusChange: (todo: any) => void;
+  key: any;
 } & HtmlHTMLAttributes<HTMLHtmlElement>;
